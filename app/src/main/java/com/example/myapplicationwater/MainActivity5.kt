8ecaf7cfbec079159e3296e6946bdc9b3002e2ac
409570418 <zzz1230626@gmail.com>
2022-12-21 21:07:28 +0800

    加入連結

--- app/src/main/java/com/example/myapplicationwater/MainActivity5.kt
+++ app/src/main/java/com/example/myapplicationwater/MainActivity5.kt
@@ -1,9 +1,10 @@
 package com.example.myapplicationwater
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.*
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,14 @@ class MainActivity5 : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_back3).setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
+
+            startActivity(intent)
             startActivityForResult(intent, 13)
+        }
+        findViewById<Button>(R.id.btn_back4).setOnClickListener {
+            val uri = Uri.parse("https://www.youtube.com/results?search_query=%E5%96%9D%E6%B0%B4%E7%9F%A5%E8%AD%98")
+            val i = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(i)
         }
     }
 }
